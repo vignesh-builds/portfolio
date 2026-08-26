5b62663bd122cc32fb922a1751310a13d1fd51c0
vignesh-builds <vignesh.builds@gmail.com>
2026-08-26 20:26:52 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -30,7 +30,7 @@ function Home() {
           </a>
 
           <a
-            href=".\public\Vignesh-Resume.pdf"
+            href="\public\Vignesh-Resume.pdf"
             target="_blank"
             rel="noreferrer"
             className="secondary-btn"
