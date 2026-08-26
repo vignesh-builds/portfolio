0984a7f971db57e26ce5e34ac302f2bef8814b0d
vignesh-builds <vigneshsankar1926@gmail.com>
2026-08-26 13:09:10 +0530

    Fix CORS for deployed frontend

--- backend/src/main/java/com/vignesh/portfolio_backend/controller/ContactController.java
+++ backend/src/main/java/com/vignesh/portfolio_backend/controller/ContactController.java
@@ -1,12 +1,17 @@
 package com.vignesh.portfolio_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.vignesh.portfolio_backend.entity.Contact;
 import com.vignesh.portfolio_backend.repository.ContactRepository;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://portfolio-frontend-evvl.onrender.com")
 public class ContactController {
 
     private final ContactRepository contactRepository;
